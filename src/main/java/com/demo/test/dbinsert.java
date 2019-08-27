933127486fd0c36298d885ab9a537c5c320d3c35
laojie <1435532727@qq.com>
2019-08-27 15:11:58 +0800

    test

---
+++ src/main/java/com/demo/test/dbinsert.java
@@ -0,0 +1,12 @@
+package com.demo.test;
+
+import org.junit.jupiter.api.Test;
+
+public class dbinsert {
+
+    @Test
+    public void test(){
+        System.out.println("adsflksadj;lfk");
+    }
+
+}
